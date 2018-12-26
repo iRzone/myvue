43043ee9bc7542a0a687716dab084180529f6c62
iRzone <753896209@qq.com>
2018-12-26 23:31:09 +0800

    安装iview UI框架和vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import iView from 'iview'
+import router from './router/index.js'
+import 'iview/dist/styles/iview.css'
+
+Vue.use(iView)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
